6862dc637fa8bacd37760ddb712dc05e0f9aee04
Ignacio_Silva <nasho16101999@gmai.com>
2021-08-31 20:12:02 -0400

    desafio 2 modulo 6 +  ejemplos

---
+++ index.js
@@ -0,0 +1,55 @@
+
+const http = require('http')
+const url = require('url')
+const fs = require('fs')
+const chalk = require('chalk')
+const { v4: uuidv4 } = require('uuid')
+
+console.log(chalk.white.bgRed.bold(('Hola Mundo!')))
+console.log(uuidv4())
+
+
+
+/*http
+    .createServer(function (req, res) {
+       
+        const params = url.parse(req.url, true).query
+
+        //create
+        const archivo = params.archivo
+        const contenido = params.contenido
+        
+        //leer
+        const newnombre = params.nuevoNombre
+       
+        if (req.url.includes('/crear')) {
+            fs.writeFile(archivo, contenido, () => {
+                res.write('Archivo creado con éxito!')
+                res.end()
+            })
+        }
+     
+        if (req.url.includes('/leer')) {
+            fs.readFile(archivo, (err, data) => {
+                res.write(data)
+                res.end()
+            })
+        }
+        
+        if (req.url.includes('/renombrar')) {
+            fs.rename(archivo, newnombre, (err, data) => {
+                res.write(`Archivo ${archivo} renombrado por ${newnombre}`)
+                res.end()
+            })
+        }
+
+        if (req.url.includes('/eliminar')) {
+            fs.unlink(archivo, (err, data) => {
+                res.write(`Archivo ${archivo} eliminado con éxito`)
+                res.end()
+         })
+        }
+    })
+    .listen(8080, () => console.log('Escuchando el puerto 8080'))
+*/
+
